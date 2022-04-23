96bb0504e0e8f1dc03daa0f78a66cd207bc2debd
levina13 <87984577+levina13@users.noreply.github.com>
2022-04-23 12:58:13 +0700

    list wisata

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -14,7 +14,8 @@ class PageController extends Controller
         ->join('kecamatans', 'objek_wisatas.kecamatan', '=', 'kecamatans.id_kecamatan')
         ->join('jenis_wisatas', 'objek_wisatas.jenis_wisata', '=', 'jenis_wisatas.id_jenis_wisata')
         ->join('gambar_objeks', 'objek_wisatas.id_gambar', '=', 'gambar_objeks.id_gambar_objek')
-        ->orderBy('objek_wisatas.created_at', 'DESC')->take(10)->get();
+        // ->orderBy('objek_wisatas.created_at', 'DESC')->take(8)->get();
+        ->inRandomOrder()->take(8)->get();
 
         $wisataPopulers = ObjekWisatas::select('objek_wisatas.*', 'kecamatans.kecamatan as nama_kecamatan', 'jenis_wisatas.jenis as nama_kategori', 'gambar_objeks.gambar as gambar')
             ->join('kecamatans', 'objek_wisatas.kecamatan', '=', 'kecamatans.id_kecamatan')
--- resources/views/pages/wisatas/list.blade.php
+++ resources/views/pages/wisatas/list.blade.php
@@ -25,12 +25,11 @@
 		</section>
 
 
-		<section class="section-popular main-section" id="popular">
+		<section class="section-popular main-section" id="popular" style="margin-bottom: -300px !important;">
 			<div class="container">
 				<div class="row">
-					<div class="col text-center section-popular-heading">
-					<h2>Destinasi Wisata</h2>
-					<p>Tempat yang belum Anda lihat sebelumnya di dunia</p>
+					<div class="col text-center section-popular-heading" >
+					<h2>Daftar Destinasi Wisata</h2>
 					</div>
 				</div>
 			</div>
@@ -39,22 +38,26 @@
 
 		<section class="section-popular-content" id="popularContent">
 			<div class="container">
-				<div class="section-popular-travel row justify-content-center">
-                    @foreach ($wisatas as $wisata)
-                        <div class="col-sm-6 col-md-4 col-lg-3">
-                            <div class="card-travel text-center d-flex flex-column" style="background-image: url('{{ urlencode($wisata->gambar) }}');">
-                                <div class="travel-country">{{ $wisata->nama_kecamatan }}</div>
-                                <div class="travel-location">{{ $wisata->nama }}</div>
-                                <div class="travel-button mt-auto">
-                                    <a href="{{ route('public.wisata', $wisata->id_objek_wisata) }}" class="btn btn-travel-details px-4">
-                                        Detail Wisata
-                                    </a>
+                <div class="card border-0 shadow">
+                    <div class="card-body">
+                        <div class="section-popular-travel row justify-content-center">
+                            @foreach ($wisatas as $wisata)
+                            <div class="col-sm-6 col-md-4 col-lg-3">
+                                <div class="border-0 shadow rounded card-travel text-center d-flex flex-column" style="background-image: url('{{ urlencode($wisata->gambar) }}');" >
+                                    <div class="travel-country">{{ $wisata->nama_kecamatan }}</div>
+                                    <div class="travel-location">{{ $wisata->nama }}</div>
+                                    <div class="travel-button mt-auto">
+                                        <a href="{{ route('public.wisata', $wisata->id_objek_wisata) }}" class="btn btn-travel-details px-4">
+                                            Detail Wisata
+                                        </a>
+                                    </div>
                                 </div>
                             </div>
+                            @endforeach
                         </div>
-                    @endforeach
-				</div>
-                <div aria-label="Page navigation example" class="row">
+                    </div>
+                </div>
+                <div aria-label="Page navigation example" class="row" style="margin-top: 20px">
                     {{ $wisatas->links() }}
                 </div>
 			</div>
